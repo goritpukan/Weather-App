8bdb2fdeae14cbd8319651888b95158130baa493
Vadym <test@gmail.com>
2024-05-07 00:15:08 +0300

    add city defenition

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ searchForm.addEventListener("submit", onSubmit);
 
 const cityInput = document.querySelector(".city");
 
+const weatherCityInfo = document.querySelector(".weather-city-info");
 const weatherCondition = document.querySelector(".weather-condition");
 const weatherTemp = document.querySelector(".weather-temp");
+
+setLocation();
+
+async function setLocation(){
+  const city = await getLocation();
+  if(city){
+    getWeather(city, language)
+  }
+}
+
+async function getLocation(){
+  const locatiomData = await fetch("https://api.ipdata.co?api-key=9a161b5d9fdd32f4441bfe6aaf32078a994e04db5a0bf26861703299");
+  const locationJson = await locatiomData.json();
+  return locationJson.city || null
+}
 
 function onSubmit(event){
   event.preventDefault();
@@ -29,9 +45,11 @@ function getWeather(city, language) {
 function setWeatherInfo(info){
   if(info.error){
     weatherTemp.innerHTML = "";
-    weatherCondition.innerHTML="Помилка! Спробуйте знову ввести місто";
+    weatherCityInfo.innerHTML="Помилка! Спробуйте знову ввести місто";
     return;
   }
+
+  weatherCityInfo.innerHTML = info.location.name
   weatherCondition.innerHTML = `Стан: ${info.current.condition.text}`;
   weatherTemp.innerHTML = `${info.current.temp_c }°C`
 }
